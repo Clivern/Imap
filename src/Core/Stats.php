34070072c2349e26863c099fc66a0e7ba7853953
Clivern <hello@clivern.com>
2017-08-10 12:17:50 +0200

    stats class updated

--- src/Core/Stats.php
+++ src/Core/Stats.php
@@ -14,25 +14,56 @@ use Clivern\Imap\Core\Connection;
  */
 class Stats
 {
+	/**
+	 * @var Connection
+	 */
 	protected $connection;
 
+	/**
+	 * Stats Class Constructor
+	 *
+	 * @param Connection $connection
+	 * @return void
+	 */
 	public function __construct(Connection $connection)
 	{
 		$this->connection = $connection;
 	}
 
+	/**
+	 * Get Quota
+	 *
+	 * @param  string $folder
+	 * @return array
+	 */
 	public function getQuota($folder = 'INBOX')
 	{
 		$data = imap_get_quotaroot($this->connection->getStream(), $folder);
 
-		return $data;
+		return [
+			'usage' => (isset($data['usage'])) ? $data['usage'] : false,
+			'limit' => (isset($data['limit'])) ? $data['limit'] : false
+		];
 	}
 
-
+	/**
+	 * Get Status
+	 *
+	 * @param  string $folder
+	 * @param  string $flag
+	 * @return array
+	 */
 	public function getStatus($folder = 'INBOX', $flag = SA_ALL)
 	{
 		$data = imap_status($this->connection->getStream(), "{" . $this->connection->getServer() . "}" . $folder, $flag);
 
-		return $data;
+		return [
+			'flags' => (isset($data->flags)) ? $data->flags : false,
+			'messages' => (isset($data->messages)) ? $data->messages : false,
+			'recent' => (isset($data->recent)) ? $data->recent : false,
+			'unseen' => (isset($data->unseen)) ? $data->unseen : false,
+			'uidnext' => (isset($data->uidnext)) ? $data->uidnext : false,
+			'uidvalidity' => (isset($data->uidvalidity)) ? $data->uidvalidity : false
+		];
 	}
 }
